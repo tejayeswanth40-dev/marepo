f56f5b5e94867ddab26e1c8f5f1f7d32116c71d5
tejayeswanth40-dev <tejayeswanth40@gmail.com>
2026-04-01 16:08:21 +0000

    Fixed errors in main.sh, added functions to main.sh, main.sh working perfectly

--- main.sh
+++ main.sh
@@ -1,92 +1,167 @@
 #!/bin/bash
 
+# Checks if the username entered is present in users.tsv
+scan_users() {
+    if grep -qE "^${1}"$'\t' users.tsv; then
+        return 0
+    else
+        return 1
+    fi
+}
+# Checks if the username and password are present in users.tsv
+pass_check() {
+    if grep -qE "^${1}"$'\t'"${2}$" users.tsv; then
+        return 0
+    else
+        return 1
+    fi
+}
 login1=false
 login2=false
 while true; do
+        # READ USERNAME AND PASSWORD FOR PLAYER 1
     read -p "Enter Player 1 username: " usrname1
-        if awk -F'\t' '$1 == "'$usrname1'" {exit 0 } END {exit 1}' users.tsv; then
+        if scan_users "$usrname1"; then
             while true; do
-                read -p "Enter Player 1 password: " pass1
+                read -s -p "Enter Player 1 password: " pass1
+                echo
+                read -s -p "Re-enter Player 1 password: " pass1_confirm
+                echo
+                if [ "$pass1" != "$pass1_confirm" ]; then
+                    echo "Passwords do not match.Please enter the password again."
+                    read -p "'x' to terminate the session and any other character to enter password again: " choice4
+                    if [ "$choice4" == "x" ]; then
+                        exit 1 # Terminate
+                    else
+                        continue # Enter password for Player 1 again
+                    fi
+                fi
                 hashpass1=$(echo -n "$pass1" | sha256sum | awk '{print $1}')
-                if  awk -F'\t' '$1 == "'$usrname1'" && $2 == "'$hashpass1'" {exit 0 } END {exit 1}' users.tsv; then
+                if pass_check "$usrname1" "$hashpass1"; then
                     echo "Player 1 logged in successfully."
                     login1=true
-                    #login if usr2 is also matching
-                    break
+                    break # Login successful for player 1
                 else
-                    echo "Incorrect password for Player 1, please enter password again."
-                    continue
+                    echo "Incorrect password for Player 1, please enter the password again."
+                    read -p "'x' to terminate the session and any other character to enter password again: " choice0
+                    if [ "$choice0" == "x" ]; then
+                        exit 1 #Terminate
+                    else
+                        continue #Enter password for Player 1 again
+                    fi
                 fi
-            done
+            done # Login loop for player 1 ENDS
         else
             echo "Username not found for Player 1, Would you like to register or enter username again."
-            read -p "Enter 'r' to register or any other character to enter username again: " choice1
-
+            read -p "Enter 'r' to register and 'x' to terminate session or any other character to enter username again: " choice1
+                # REGISTERATION FOR PLAYER 1
             if [ "$choice1" == "r" ]; then
                 while true; do
-                    # Registration process for Player 1
                     read -p "Enter a new username for Player 1: " usrname1
-                    if awk -F'\t' '$1 == "'$usrname1'" {exit 0 } END {exit 1}' users.tsv ; then
+                    if scan_users "$usrname1"; then
                         echo "Username already exists. Please choose a different username."
                         continue
                     else
-                        read -p "Enter a new password for Player 1: " pass1
+                        read -s -p "Enter a new password for Player 1: " pass1
+                        echo
+                        read -s -p "Re-enter the new password for Player 1: " pass1_confirm
+                        echo
+                        if [ "$pass1" != "$pass1_confirm" ]; then
+                            echo "Passwords do not match. Please enter the new password again."
+                            read -p "'x' to terminate the session and any other character to enter password again: " choice5
+                            if [ "$choice5" == "x" ]; then
+                                exit 1 # Terminate
+                            else
+                                continue # Enter new password for Player 1 again
+                            fi
+                        fi
                         hashpass1=$(echo -n "$pass1" | sha256sum | awk '{print $1}')
                         echo -e "$usrname1\t$hashpass1" >> users.tsv
                         echo "Player 1 registered successfully."
                         login1=true
-                        break
+                        break # Registered successfully for player 1
                     fi
                 done
                 break # Exit the first loop after successful registration
-            elif [ "$choice1" != "r" ]; then
-                # Loop back to username input for Player 1
+            elif [ "$choice1" == "x" ]; then
+                exit 1 # Terminate
+            else
                 echo "Fresh Start"
-                continue
+                continue # Go back to the start, enter username for player 1
             fi
         fi
     break # Exit the first loop after successful login
 done
+        # READ USERNAME AND PASSWORD FOR PLAYER 2
 while true; do
     read -p "Enter Player 2 username: " usrname2
-        if awk -F'\t' '$1 == "'$usrname2'" {exit 0 } END {exit 1}' users.tsv && $usrname2 != $usrname1 ; then
+        if scan_users "$usrname2" && [ "$usrname2" != "$usrname1" ]; then
             while true; do
-                read -p "Enter Player 2 password: " pass2
+                read -s -p "Enter Player 2 password: " pass2
+                echo
+                read -s -p "Re-enter Player 2 password: " pass2_confirm
+                echo
+                if [ "$pass2" != "$pass2_confirm" ]; then
+                    echo "Passwords do not match. Please enter the password again."
+                    read -p "'x' to terminate the session and any other character to enter password again: " choice6
+                    if [ "$choice6" == "x" ]; then
+                        exit 1 # Terminate
+                    else
+                        continue # Enter password for Player 2 again
+                    fi
+                fi
                 hashpass2=$(echo -n "$pass2" | sha256sum | awk '{print $1}')
-                if awk -F'\t' '$1 == "'$usrname2'" && $2 == "'$hashpass2'" {exit 0 } END {exit 1}' users.tsv; then
+                if pass_check "$usrname2" "$hashpass2"; then
                     echo "Player 2 logged in successfully."
                     login2=true
-                    break
+                    break # Login successful for player 2
                 else
                     echo "Incorrect password for Player 2, please enter password again."
-                    continue
+                    read -p "'x' to terminate the session and any other character to enter password again: " choice3
+                    if [ "$choice3" == "x" ]; then
+                        exit 1 # Terminate
+                    else
+                        continue # Enter password for Player 2 again
+                    fi
                 fi
             done
         else
             echo "Username not found for Player 2 or username same as Player 1, Would you like to register or enter username again."
-            read -p "Enter 'r' to register or any other character to enter username again: " choice2
-
+            read -p "Enter 'r' to register and 'x' to terminate the session or any other character to enter username again: " choice2
+                    # REGISTERATION FOR PLAYER 2
             if [ "$choice2" == "r" ]; then
                 while true; do
-                    # Registration process for Player 2
                     read -p "Enter a new username for Player 2: " usrname2
-                    if awk -F'\t' '$1 == "'$usrname2'" {exit 0 } END {exit 1}' users.tsv; then
+                    if scan_users "$usrname2"; then
                         echo "Username already exists. Please choose a different username."
                         continue
                     else
-                        read -p "Enter a new password for Player 2: " pass2
+                        read -s -p "Enter a new password for Player 2: " pass2
+                        echo
+                        read -s -p "Re-enter the new password for Player 2: " pass2_confirm
+                        echo
+                        if [ "$pass2" != "$pass2_confirm" ]; then
+                            echo "Passwords do not match. Please enter the new password again."
+                            read -p "'x' to terminate the session and any other character to enter password again: " choice7
+                            if [ "$choice7" == "x" ]; then
+                                exit 1 # Terminate
+                            else
+                                continue # Enter new password for Player 2 again
+                            fi
+                        fi
                         hashpass2=$(echo -n "$pass2" | sha256sum | awk '{print $1}')
                         echo -e "$usrname2\t$hashpass2" >> users.tsv
                         echo "Player 2 registered successfully."
                         login2=true
-                        break
+                        break # Registered successfully for player 2
                     fi
                 done
                 break # Exit the first loop of usrname2 after successful registration
-            elif [ "$choice2" != "r" ]; then
-                # Loop back to username input for Player 2
+            elif [ "$choice2" == 'x' ]; then
+                exit 1 # Terminate
+            else
                 echo "Fresh Start"
-                continue
+                continue # Go back to the start, enter username for player 2
             fi
         fi
     break # Exit the first loop of usrname2 after successful login
