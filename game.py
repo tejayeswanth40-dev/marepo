2dd68215d9f13a425da1959195e7157acae68bb9
tejayeswanth40-dev <tejayeswanth40@gmail.com>
2026-04-15 18:23:26 +0000

    Made a structured base class in game.py and sub class in C4 and T3

--- game.py
+++ game.py
@@ -1,14 +1,16 @@
 import numpy as np
+import tictactoe.py as t3
+import connect4.py as c4
 
 class BaseGame:
-    def __init__(self, usrname1, usrname2, rows, cols, game_name):
+    def __init__(self, usrname1, usrname2, gameclass):
         self.player1 = usrname1
         self.player2 = usrname2
-        self.rows = rows
-        self.cols = cols
-        self.game = game_name 
+        self.board = gameclass.generate_board()
         self.current_turn = 1
-    
+        self.game = gameclass
+        self.winner = 0
+
     def switch_turn(self):
         if self.current_turn == 1:
             self.current_turn = 2
--- games/tictactoe.py
+++ games/tictactoe.py
@@ -3,9 +3,17 @@ import numpy as np
 
 class T3(BaseGame):
     def __init__(self, usrname1, usrname2):
-        super().__init__(usrname1, usrname2, 10, 10, "Tic Tac Toe")
-        #not over yet
-        #BOARD IS NOT YET DEFINED WILL DEFINE LATER AS PER THE REQUIREMENTS
+        super().__init__(usrname1, usrname2, T3)
+
+    def generate_board(self):
+        return np.zeros((10,10), dtype=int)
+
+    def make_move(self, row, col, turn):
+        if self.board[row, col] != 0:
+            return False
+        else:
+            self.board[row, col] = turn
+            return True
     def check_win(self):
         if np.all(self.board[0,:] == 1):
             return 1
